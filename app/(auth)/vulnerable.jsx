d3e323ae3e7212af70410b26d4b12f94d28439dd
Mark Aldwin <acdwinwin@gmail.com>
2025-07-30 20:50:01 +0800

    feat: updates for Dwin Branch - Added Appwrite configuration - Added Android package configuration - Added comprehensive code documentation - Added image picker dependency - Implemented registration flow

---
+++ app/(auth)/vulnerable.jsx
@@ -0,0 +1,15 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+const Vulnerable = () => {
+    //form field state variables
+
+
+
+    return (
+        <View>
+            <Text>Vulnerable</Text>
+        </View>
+    )
+}
+export default Vulnerable
